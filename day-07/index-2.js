d96bd0b8e66c2737d498f57fd0a92f0a6690149f
Andrew Goodman <github@gdman.co.uk>
2024-12-07 15:12:34 +0000

    Day 7 Part 2

---
+++ day-07/index-2.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+const formulas = fs.readFileSync(__dirname + '/input.txt', 'utf8').split('\n').map(line => {
+    const [ total, numbers ] = line.split(':');
+    return {
+        total : parseInt(total),
+        numbers : numbers.trim().split(' ').map(number => parseInt(number))
+    };
+});
+
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const contat = (a, b) => parseInt(a + '' + b);
+
+const operations = [ add, multiply, contat ];
+
+const isPossible = (total, numbers) => {
+    const sums = [ [ numbers.shift() ] ];
+
+    for (let i = 0; i < numbers.length; i++) {
+        const number = numbers[i];
+
+        const nextIndex = i + 1;
+
+        sums[nextIndex] = [];
+
+        for (const previousTotal of sums[i]) {
+            for (const operation of operations) {
+                const newTotal = operation(previousTotal, number);
+
+                if (newTotal > total) {
+                    continue;
+                }
+
+                sums[nextIndex].push(newTotal);
+            }
+        }
+    }
+
+    return sums[sums.length - 1].includes(total);
+}
+
+let grandTotal = 0;
+
+for (const { total, numbers } of formulas) {
+    if (isPossible(total, numbers)) {
+        grandTotal += total;
+    }
+}
+
+console.log(grandTotal);
