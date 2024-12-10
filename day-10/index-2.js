624fd2ee5d669aa2d9fd39fba92976873cb23bc9
Andrew Goodman <github@gdman.co.uk>
2024-12-10 15:04:18 +0000

    Day 10 Part 2 - really lazy solution

---
+++ day-10/index-2.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+
+const grid = fs.readFileSync(__dirname + '/input.txt', 'utf8').split('\n').map(line => line.split('').map(num => parseInt(num)));
+
+const directions = [ [ -1, 0 ], [ 0, 1 ], [ 1, 0 ], [ 0, -1 ] ];
+
+const getNextCells = (requiredHeight, [ row, col ]) => {
+    const nextCells = [];
+
+    for (const [ rowDiff, colDiff ] of directions) {
+        const newRow = row + rowDiff;
+        const newCol = col + colDiff;
+
+        if (grid[newRow]?.[newCol] === requiredHeight) {
+            nextCells.push([ newRow, newCol ]);
+        }
+    }
+
+    return nextCells;
+}
+
+let globalNumberOfPaths = 0;
+
+const getReachableTrailheads = (height, [ row, col ]) => {
+    if (height === 9) {
+        globalNumberOfPaths++;
+        return new Set([ row + ':' + col ]);
+    }
+
+    const nextCells = getNextCells(height + 1, [ row, col ]);
+
+    if (nextCells.length === 0) {
+        return new Set();
+    }
+
+    let allReachableTrailHeads = new Set();
+
+    for (const nextCell of nextCells) {
+        const reachableTrailHeads = getReachableTrailheads(height + 1, nextCell);
+
+        if (reachableTrailHeads.size > 0) {
+            reachableTrailHeads.forEach(reachableTrailHead => allReachableTrailHeads.add(reachableTrailHead));
+        }
+    }
+    return allReachableTrailHeads;
+}
+
+for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+        const height = grid[row][col];
+
+        if (height !== 0) {
+            continue;
+        }
+
+        getReachableTrailheads(0, [ row, col ]);
+    }
+}
+
+console.log(globalNumberOfPaths);
